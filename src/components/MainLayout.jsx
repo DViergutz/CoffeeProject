2956bc7daf80286572cb355b3413b9d074e28469
DViergutz <d.viergutz@hotmail.de>
2024-03-28 17:59:53 -0300

    navbar y productDetail

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navigation from "./Navbar";
 import Footer from "./Footer";
 
 function MainLayout() {
   return (
     <div>
-      <Navbar />
+      <Navigation />
       <Outlet />
       <Footer />
     </div>
