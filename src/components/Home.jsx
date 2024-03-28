a8d49f402fcda9d725abc6134168dd4fd612c968
Santiago T <terka_ternande@hotmail.com>
2024-03-28 15:06:07 -0300

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,72 @@ function Home() {
   return (
     <div>
       <div className="hero-section">
-        <div className="hero-info">
-          <h1>Estamos en la Home</h1>
-          <p>
+        <div className="content-wrapper">
+          <h1>Discover the Essence of Fresh Coffee</h1>
+          <p className="hero-subheading">
             Embark on a journey through our finest selection of coffee bean bags
           </p>
           <button className="btn btn-primary">Explore our products</button>
+        </div>
+      </div>
+      <div className="main-section">
+        <h2>Our Categories</h2>
+        <div className="row">
+          <div className="col">
+            <div className="category">
+              <h4>Flavored</h4>
+            </div>
+          </div>
+          <div className="col">
+            <div className="category">
+              <h4>Blends</h4>
+            </div>
+          </div>
+          <div className="col">
+            <div className="category">
+              <h4>Single Origin</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-section">
+        <h2>Special Products</h2>
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-3">
+            <div className="category">
+              <h4>Flavored</h4>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="category">
+              <h4>Blends</h4>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="category">
+              <h4>Single Origin</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-section">
+        <h2>Latest Blogs</h2>
+        <div className="row">
+          <div className="col">
+            <div className="category">
+              <h4>Flavored</h4>
+            </div>
+          </div>
+          <div className="col">
+            <div className="category">
+              <h4>Blends</h4>
+            </div>
+          </div>
+          <div className="col">
+            <div className="category">
+              <h4>Single Origin</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
