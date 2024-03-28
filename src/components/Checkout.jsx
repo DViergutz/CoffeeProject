0919be2c47a8b763f0679e1551f940656dfbf4fd
Santiago T <terka_ternande@hotmail.com>
2024-03-28 16:49:22 -0300

    checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,8 +5,111 @@ function Checkout() {
     <div>
       <div className="main-section">
         <h2>Shopping Cart</h2>
+        <hr />
         <div className="row">
-          <div className="col-8"></div>
+          <div className="col-8 bg-light border rounded pt-4">
+            <div className="d-flex justify-content-evenly pb-3">
+              <div className="col w-25 fw-semibold text-center">PRODUCT</div>
+              <div className="col w-25 fw-semibold text-center">PRICE</div>
+              <div className="col w-25 fw-semibold text-center">QUANTITY</div>
+              <div className="col w-25 fw-semibold text-center">TOTAL</div>
+            </div>
+            <div className="d-flex py-4 border-top">
+              <div className="col w-25 fw-semibold  d-flex justify-content-center align-items-center img-checkout"></div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column align-items-center">
+                  <span>Quantity</span>
+                  <div className="d-flex border p-1">
+                    <span>-</span>
+                    <span className="mx-3">1</span>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+            </div>
+            <div className="d-flex py-4 border-top">
+              <div className="col w-25 fw-semibold  d-flex justify-content-center align-items-center img-checkout"></div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column align-items-center">
+                  <span>Quantity</span>
+                  <div className="d-flex border p-1">
+                    <span>-</span>
+                    <span className="mx-3">1</span>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+            </div>
+            <div className="d-flex py-4 border-top">
+              <div className="col w-25 fw-semibold  d-flex justify-content-center align-items-center img-checkout"></div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column align-items-center">
+                  <span>Quantity</span>
+                  <div className="d-flex border p-1">
+                    <span>-</span>
+                    <span className="mx-3">1</span>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+            </div>
+            <div className="d-flex py-4 border-top">
+              <div className="col w-25 fw-semibold  d-flex justify-content-center align-items-center img-checkout"></div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column align-items-center">
+                  <span>Quantity</span>
+                  <div className="d-flex border p-1">
+                    <span>-</span>
+                    <span className="mx-3">1</span>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+            </div>
+            <div className="d-flex py-4 border-top">
+              <div className="col w-25 fw-semibold  d-flex justify-content-center align-items-center img-checkout"></div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column align-items-center">
+                  <span>Quantity</span>
+                  <div className="d-flex border p-1">
+                    <span>-</span>
+                    <span className="mx-3">1</span>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col w-25 fw-semibold d-flex justify-content-center align-items-center">
+                $500.00
+              </div>
+            </div>
+          </div>
           <div className="col-4"></div>
         </div>
       </div>
