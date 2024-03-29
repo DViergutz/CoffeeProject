963a8cd57ca5a15b5ef047b50888f6a33929d72a
Peter Slowak <p_slowak89@hotmail.com>
2024-03-29 10:47:56 -0300

    scroll to top button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { useEffect, useRef, useState } from "react";
+import ScrollToTop from "./ScrollToTop";
+import "../App.css";
 
 function Home() {
+  const categories = useRef(null);
+  const specialProducts = useRef(null);
+  const blog = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
-      <div className="hero-section">
+      <ScrollToTop />
+      <div className="navigation-menu">
+        <ul>
+          <li onClick={() => scrollToSection(categories)} className="nav-link">
+            Categories
+          </li>
+          <li
+            onClick={() => scrollToSection(specialProducts)}
+            className="nav-link"
+          >
+            Special Products
+          </li>
+          <li onClick={() => scrollToSection(blog)} className="nav-link">
+            Blog
+          </li>
+        </ul>
+      </div>
+      <div id="hero" className="hero-section">
         <div className="content-wrapper">
           <h1>Discover the Essence of Fresh Coffee</h1>
           <p className="hero-subheading">
@@ -12,7 +43,7 @@ function Home() {
           <button className="btn btn-primary">Explore our products</button>
         </div>
       </div>
-      <div className="main-section">
+      <div id="categories" ref={categories} className="main-section">
         <h2>Our Categories</h2>
         <div className="row">
           <div className="col">
@@ -32,7 +63,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="main-section">
+      <div id="special-products" ref={specialProducts} className="main-section">
         <h2>Special Products</h2>
         <div className="row d-flex justify-content-center">
           <div className="col-lg-3">
@@ -52,7 +83,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="main-section">
+      <div id="blog" ref={blog} className="main-section">
         <h2>Latest Blogs</h2>
         <div className="row">
           <div className="col">
