4161ce8abdeb8347467700676975ad2237400e6f
DViergutz <d.viergutz@hotmail.de>
2024-03-29 10:53:31 -0300

    one product fetch

--- src/routes.jsx
+++ src/routes.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
         element: <ProductDetail />,
       },
       {
-        path: "/categorys/:categoryName",
+        path: "/categories/:categoryName",
         element: <ProductsCategory />,
       },
       {
