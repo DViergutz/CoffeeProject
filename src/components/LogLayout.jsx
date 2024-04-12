175475314d25bf0518e87102133f5e8ab5260628
Peter Slowak <p_slowak89@hotmail.com>
2024-04-12 13:32:53 -0300

    Update LogLayout.jsx

--- src/components/LogLayout.jsx
+++ src/components/LogLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import CartOffCanvas from "./CartOffCanvas";
 import Navigation from "./Navbar";
 
 function LogLayout() {
   return (
     <div>
       <Navigation />
+      <CartOffCanvas />
       <Outlet />
     </div>
   );
