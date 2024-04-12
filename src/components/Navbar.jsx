9b05537eae80dd5e841ec0a491ba9725f5191569
Santiago T. <terka_ternande@hotmail.com>
2024-04-12 11:20:03 -0300

    d

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,67 +30,91 @@ function Navigation() {
           className="text-orange bg-light"
         />
 
-        {isLogged ? (
-          <Navbar.Collapse
-            id="responsive-navbar-nav"
-            className="collapse ps-3 bg-dark-grey"
-          >
-            <Nav className="me-auto">
-              <Nav.Link
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="collapse ps-3 bg-dark-grey"
+        >
+          <Nav className="me-auto">
+            <Nav.Link
+              as={Link}
+              to="/about-this-project"
+              className="navLink"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              ABOUT THIS PROJECT
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/products"
+              className="navLink"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              PRODUCTS
+            </Nav.Link>
+            <NavDropdown
+              className="navLink  navDropdown"
+              title="CATEGORIES"
+              id="navbarScrollingDropdown"
+            >
+              <NavDropdown.Item as={Link} to="/category/Flavored" className="">
+                Flavored
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/category/Blends" className="">
+                Blends
+              </NavDropdown.Item>
+              <NavDropdown.Item
                 as={Link}
-                to="/about-this-project"
-                className="navLink"
+                to="/category/Single Origin"
+                className=""
                 onClick={() => window.scrollTo(0, 0)}
               >
-                ABOUT THIS PROJECT
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/products"
-                className="navLink"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                PRODUCTS
-              </Nav.Link>
-              <NavDropdown
-                className="navLink  navDropdown"
-                title="CATEGORIES"
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Item
-                  as={Link}
-                  to="/category/Flavored"
-                  className=""
-                >
-                  Flavored
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/category/Blends" className="">
-                  Blends
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  as={Link}
-                  to="/category/Single Origin"
-                  className=""
-                  onClick={() => window.scrollTo(0, 0)}
-                >
-                  Single Origin
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+                Single Origin
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
 
-            <Nav className="d-flex align-items-start align-items-center ">
-              <i
-                className="bi bi-cart2 fs-5 text-orange ms-2 navLink"
-                onClick={() => dispatch(setIsCartOpen())}
-              ></i>
-              <Nav.Link
-                as={Link}
-                to="/admin/login"
-                className="navLink"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                ADMIN
-              </Nav.Link>
+          <Nav className="d-flex align-items-start align-items-center ">
+            <NavDropdown
+              className="navLink navDropdown"
+              title={<i className="bi bi-person-fill fs-5 "></i>}
+              id="navbarScrollingDropdown"
+            >
+              {isLogged ? (
+                <></>
+              ) : (
+                <>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/user/login"
+                    className=""
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    Login
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/user/register"
+                    className=""
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    Register
+                  </NavDropdown.Item>
+                </>
+              )}
+            </NavDropdown>
+            <i
+              className="bi bi-cart2 fs-5 text-orange ms-2 navLink"
+              onClick={() => dispatch(setIsCartOpen())}
+            ></i>{" "}
+            <Nav.Link
+              as={Link}
+              to="/admin/login"
+              className="navLink"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              ADMIN
+            </Nav.Link>
+            {isLogged ? (
               <Nav.Link
                 as={Link}
                 to="/"
@@ -104,94 +128,11 @@ function Navigation() {
                   LOGOUT
                 </button>
               </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        ) : (
-          <Navbar.Collapse
-            id="responsive-navbar-nav"
-            className="collapse ps-3 bg-dark-grey"
-          >
-            <Nav className="me-auto">
-              <Nav.Link
-                as={Link}
-                to="/about-this-project"
-                className="navLink"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                ABOUT THIS PROJECT
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/products"
-                className="navLink"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                PRODUCTS
-              </Nav.Link>
-              <NavDropdown
-                className="navLink  navDropdown"
-                title="CATEGORIES"
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Item
-                  as={Link}
-                  to="/category/Flavored"
-                  className=""
-                >
-                  Flavored
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/category/Blends" className="">
-                  Blends
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  as={Link}
-                  to="/category/Single Origin"
-                  className=""
-                  onClick={() => window.scrollTo(0, 0)}
-                >
-                  Single Origin
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-
-            <Nav className="d-flex align-items-start align-items-center ">
-              <NavDropdown
-                className="navLink navDropdown"
-                title={<i className="bi bi-person-fill fs-5 "></i>}
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Item
-                  as={Link}
-                  to="/user/login"
-                  className=""
-                  onClick={() => window.scrollTo(0, 0)}
-                >
-                  Login
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  as={Link}
-                  to="/user/register"
-                  className=""
-                  onClick={() => window.scrollTo(0, 0)}
-                >
-                  Register
-                </NavDropdown.Item>
-              </NavDropdown>
-              <i
-                className="bi bi-cart2 fs-5 text-orange ms-2 navLink"
-                onClick={() => dispatch(setIsCartOpen())}
-              ></i>{" "}
-              <Nav.Link
-                as={Link}
-                to="/admin/login"
-                className="navLink"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                ADMIN
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        )}
+            ) : (
+              <></>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
