0a563a4802c8f3ec3a03ff7dd638e2b01c299a98
Santiago T. <terka_ternande@hotmail.com>
2024-04-16 16:36:55 -0300

    thank-you

--- src/components/ThankYouForYourBuy.jsx
+++ src/components/ThankYouForYourBuy.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import Logo from "./Logo";
+import { Link } from "react-router-dom";
 
 function ThankYouForYourBuy() {
   const logoRef = useRef(null);
@@ -16,24 +17,39 @@ function ThankYouForYourBuy() {
 
   return (
     <div className="background-thank">
-      <div className="thank-div main container ">
-        {" "}
-        <h5 className="my-5" style={{ maxWidth: "900px", lineHeight: "1.5" }}>
-          Coffee <span className="text-orange">order confirmed!</span> Dive into
-          virtual coffee art. While you wait{" "}
-          <span className="text-orange"> Hover </span> over our featured image
-          and stir your creativity!
-        </h5>
-        <div
-          className=" logo-container"
-          ref={logoRef}
-          onMouseMove={(event) => handleMouseMove(event)}
-        >
-          <Logo cursor={cursor} logoRef={logoRef} />
-
-          <button className="btn btn-view-product height65">
-            Continue Brewing!
-          </button>
+      <div className=" main container ">
+        <div className=" special-product-card-tank-you d-flex flex-column align-items-center justify-content-center ">
+          <h2>Thank you!!</h2>
+          <p className="mt-4 mb-4 fs-2 fw-semibold">
+            Your coffee <span className="text-orange">order is confirmed!</span>
+          </p>
+          <p className="fs-5">Dive into virtual coffee art.</p>
+          <p className="fs-5 text-center">
+            While you wait
+            <span className="text-orange"> Hover </span> over our featured image
+            and stir your creativity!
+          </p>
+          <div
+            className="logo-container"
+            ref={logoRef}
+            onMouseMove={(event) => handleMouseMove(event)}
+          >
+            <Logo cursor={cursor} logoRef={logoRef} />
+          </div>
+          <div className="mt-4 d-flex flex-column align-items-center justify-content-center">
+            <p className="fs-4 mb-4  text-center">
+              Discover new flavors and keep the caffeine journey going!
+            </p>
+            <Link
+              to="/"
+              className="text-decoration-none"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              <button className="btn-hero">
+                <i className="bi bi-arrow-left"></i> Let's Brew!
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
